39aa10a21d2392ec52577127575397dbf4e3ae8b
AlSakharoB <71816331+AlSakharoB@users.noreply.github.com>
2020-12-09 23:42:26 +0300

    Update pa_pb.py

--- pa_pb.py
+++ pa_pb.py
@@ -1,20 +1,22 @@
+import split as ft4
+
+
 def ft_rev_list(mass):
-    for i in range(len(mass) - 1, -1, -1):
-        mass.append(mass.pop(i))
-    return mass
+    a = mass[len(mass) - 1::-1]
+    return a
 
 
 def pa(b, a):
-    if len(b) > 0:
-        d = b.pop(0)
+    if ft4.ft_len(b) > 0:
+        d = b[0]
         a = ft_rev_list(a)
         a.append(d)
         a = ft_rev_list(a)
 
 
 def pb(a, b):
-    if len(a) > 0:
-        d = a.pop(0)
+    if ft4.ft_len(a) > 0:
+        d = a[0]
         b = ft_rev_list(b)
         b.append(d)
         b = ft_rev_list(b)
