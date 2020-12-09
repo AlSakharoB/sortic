7bf56e55c429996f4ff02252049ac9058fc5427f
AlSakharoB <71816331+AlSakharoB@users.noreply.github.com>
2020-12-09 23:42:49 +0300

    Update ra_rb_rr.py

--- ra_rb_rr.py
+++ ra_rb_rr.py
@@ -1,14 +1,13 @@
 def ra(mass):
-    a = mass.pop(0)
+    a = mass[0]
     mass.append(a)
 
 
 def rb(mass):
-    b = mass.pop(0)
+    b = mass[0]
     mass.append(b)
 
 
 def rr(a, b):
     ra(a)
     rb(b)
-
